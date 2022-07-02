dd4318069f03a915ef8872d417268ea9391628a7
Isaac Saunders <IsaacLSaunders@gmail.com>
2022-07-01 23:02:13 -0400

    observables

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   //HeroesComponent was auto imported into the app module when we used the Angluar CLI to declare the component
   //When you use the CLI to create a component it auto imports to the parent component
   //it also auto adds HeroesComponent to the declarations array below 
@@ -13,7 +14,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   //the imports array contains all external modules that the app needs
   imports: [
